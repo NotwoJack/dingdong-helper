2ab52d9b7bf1252e7c1b154a4e3b37e07f030c95
zhangyibo <598047103>
2022-04-14 19:53:05 +0800

    self setting

--- src/main/java/UserConfig.java
+++ src/main/java/UserConfig.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class UserConfig {
 
     //收货地址id
-    public static final String addressId = "";
+    public static final String addressId = "5cbe9ab5b3e6fe5bf6b81e30";
 
     /**
      * 提前获取收货地址id 填写到addressId上 规则为该站点可送达的默认收货地址  如果没有请自行去APP中设置好再运行
@@ -25,20 +25,20 @@ public class UserConfig {
      */
     public static Map<String, String> getHeaders() {
         Map<String, String> headers = new HashMap<>();
-        headers.put("ddmc-city-number", "");
-        headers.put("ddmc-build-version", "");
-        headers.put("ddmc-device-id", "");
-        headers.put("ddmc-station-id", "");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        headers.put("ddmc-city-number", "0101");
+        headers.put("ddmc-build-version", "2.82.0");
+        headers.put("ddmc-device-id", "osP8I0Ye4h8s02pKunY83P4e9Sc8");
+        headers.put("ddmc-station-id", "5b8d22f8c0a1ea3a278b8b7c");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
         headers.put("ddmc-channel", "applet");
         headers.put("ddmc-os-version", "[object Undefined]");
         headers.put("ddmc-app-client-id", "4");
-        headers.put("cookie", "");//用户是否登录关键信息 请仔细检查
+        headers.put("cookie", "DDXQSESSID=d9d9e718795cd0c18e8add403069b18a");
         headers.put("ddmc-ip", "");
-        headers.put("ddmc-longitude", "");
-        headers.put("ddmc-latitude", "");
-        headers.put("ddmc-api-version", "");
-        headers.put("ddmc-uid", "");
-        headers.put("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800123c) NetType/WIFI Language/zh_CN");//有反馈说PC端小程序的UserAgent不能下单 如果是PC端抓包直接使用此Agent即可
+        headers.put("ddmc-longitude", "121.514587");
+        headers.put("ddmc-latitude", "31.230208");
+        headers.put("ddmc-api-version", "9.49.2");
+        headers.put("ddmc-uid", "5cbe99c77d6690ee09a7ef36");
+        headers.put("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800123c) NetType/WIFI Language/zh_CN");
         headers.put("referer", "https://servicewechat.com/wx1e113254eda17715/422/page-frame.html");
         return headers;
     }
@@ -50,20 +50,20 @@ public class UserConfig {
      */
     public static Map getBody() {
         Map body = new HashMap<>();
-        body.put("uid", "");
-        body.put("longitude", "");
-        body.put("latitude", "");
-        body.put("station_id", "");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
-        body.put("city_number", "");
-        body.put("api_version", "");
-        body.put("app_version", "");
+        body.put("uid", "5cbe99c77d6690ee09a7ef36");
+        body.put("longitude", "121.514587");
+        body.put("latitude", "31.230208");
+        body.put("station_id", "5b8d22f8c0a1ea3a278b8b7c");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        body.put("city_number", "0101");
+        body.put("api_version", "9.49.2");
+        body.put("app_version", "2.82.0");
         body.put("applet_source", "");
         body.put("channel", "applet");
         body.put("app_client_id", "4");
         body.put("sharer_uid", "");
-        body.put("openid", "");
+        body.put("openid", "osP8I0Ye4h8s02pKunY83P4e9Sc8");
         body.put("h5_source", "");
-        body.put("device_token", "");//用户是否登录关键信息 请仔细检查
+        body.put("device_token", "WHJMrwNw1k/FKPjcOOgRd+B/G6PKbViqJisBpZ3wkZTq/ZmFjRjGVxB7b+QlrWQZh5+mEKcOg67Fb0d26LtxgVf8dBQXA0fXgdCW1tldyDzmauSxIJm5Txg==1487582755342");
         return body;
     }
 
